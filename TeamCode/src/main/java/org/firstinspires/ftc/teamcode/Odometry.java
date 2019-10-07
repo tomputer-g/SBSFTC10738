1d57ee5fd0c2ec3091dc8a58adc39a6e654264e3
ImagTachyon <44809267+JerryWan83274@users.noreply.github.com>
2019-10-07 12:28:15 -0400

    Update Odometry.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
@@ -1,16 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 //@Autonomous(name = "SplineTest")
 public class AAA_AdvRobotics_SplineTest extends BaseOpMode{
-    private Vector p,v1;
+    private Vector2 p,v1;
     private double theta;
     @Override
-    public void init(){
-        p=new Vector(0,0);
+    public void init(){p=new Vector2()};
+    }
+    protected void pup_date(a,b){
+        p.add(new Vector2(a*Math.cos(theta),a*Math.sin(theta)));
+        p.add(new Vector2(b*Math.cos(theta+Math.Pi/2),b*Math.sin(theta+Math.Pi/2)));
     }
     @Override
     public void loop(){
-        
-        p.add(new Vector(a*Math.cos(theta),a*Math.sin(theta)));
-        p.add(new Vector(b*Math.cos(theta+Math.Pi/2),b*Math.sin(theta+Math.Pi/2)));
+        pup_date();
     }
 }
