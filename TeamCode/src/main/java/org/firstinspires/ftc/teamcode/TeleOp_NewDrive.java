dd6e2fabc6bb19bf4567296b88186536ee6a2a9f
TomGao2001 <gzm2001@outlook.com>
2019-10-09 19:36:40 -0400

    last class + donut drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_NewDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_NewDrive.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
+
 public class TeleOp_NewDrive extends BaseOpMode {
 
     protected static final double ctrl_deadzone = 0.2;
-    protected static final double[] movement_power_params = {.25, .6, .25, 1};
+    protected static final double[] movement_power_params = {.25, 8/14, .25, 1};//TODO: scaling power makes spin bad
+
 
     @Override
     public void init() {
@@ -15,7 +16,7 @@ public class TeleOp_NewDrive extends BaseOpMode {
 
     @Override
     public void loop() {
-        scaledMove(custom_quad(this.gamepad1.left_trigger > 0.5,-this.gamepad1.left_stick_x), custom_quad(this.gamepad1.left_trigger > 0.5,-this.gamepad1.left_stick_y), custom_linear(this.gamepad1.left_trigger > 0.5,-this.gamepad1.right_stick_x)*0.6);
+        move(custom_quad(this.gamepad1.left_trigger > 0.5,-this.gamepad1.left_stick_x), custom_quad(this.gamepad1.left_trigger > 0.5,-this.gamepad1.left_stick_y), custom_linear(this.gamepad1.left_trigger > 0.5,-this.gamepad1.right_stick_x)*0.6);
         telemetry.update();
     }
 
