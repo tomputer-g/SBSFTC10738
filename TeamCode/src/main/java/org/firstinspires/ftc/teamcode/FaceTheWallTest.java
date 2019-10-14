14c14efc403341332a2f24e6dc4df3db9acf8616
soxehli <fangzhong.lyu@sbs.org>
2019-10-14 13:03:11 -0400

    hj

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FaceTheWallTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FaceTheWallTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -11,14 +12,17 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class FaceTheWallTest extends BaseAuto {
     ModernRoboticsI2cRangeSensor rangeSensorFront, rangeSensorSide;
+    Rev2mDistanceSensor left,right;
     ElapsedTime t;
-    double distFront, distSide, diff;
+    double distFront, distSide, diff,distLeft,distRight;
     @Override
     public void init() {
         initDrivetrain();
         t = new ElapsedTime();
         //rangeSensorFront = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "front");
         //rangeSensorSide = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "side");
+        left = hardwareMap.get(Rev2mDistanceSensor.class,"left");
+        right = hardwareMap.get(Rev2mDistanceSensor.class,"right");
         initIMU();
 
     }
@@ -30,27 +34,28 @@ public class FaceTheWallTest extends BaseAuto {
             while(this.gamepad1.left_bumper);
             //distFront = rangeSensorFront.getDistance(DistanceUnit.INCH);
             //distSide = rangeSensorSide.getDistance(DistanceUnit.INCH);
-            /*while(!(near(distFront,12,0.3))){
+            distLeft = left.getDistance(DistanceUnit.INCH);
+            while(!(near(distLeft,12,0.3))){
                 setAllDrivePower(0.5,0.5,-0.5,-0.5);
-                distFront = rangeSensorFront.getDistance(DistanceUnit.INCH);
+                //distFront = rangeSensorFront.getDistance(DistanceUnit.INCH);
                 //distSide = rangeSensorSide.getDistance(DistanceUnit.INCH);
-            }*/
-            turn(90,0.5,3);
+                distLeft = left.getDistance(DistanceUnit.INCH);
+            }
+            turn(90,0.3,3);
             /*
             imuHeading=0;
             while(!near(imuHeading,90,2)){
                 setAllDrivePower(0.5,0.5,-0.5,-0.5);
             }
             */
-            /*
-            while(!(near(distFront,18,0.3))){
-                distFront = rangeSensorFront.getDistance(DistanceUnit.INCH);
-                distSide = rangeSensorSide.getDistance(DistanceUnit.INCH);
-//                diff = Math.min(0.2,Math.max((distSide-12)/10,-0.2))/2;
-//                setAllDrivePower(0.5+diff,0.5+diff,-0.5-diff,-0.5-diff);
-            }
 
-             */
+            while(!(near(distLeft,18,0.3))){
+                //distFront = rangeSensorFront.getDistance(DistanceUnit.INCH);
+                //distSide = rangeSensorSide.getDistance(DistanceUnit.INCH);
+//                diff = Math.min(0.2,Math.max((distSide-12)/10,-0.2))/2;
+                distLeft = left.getDistance(DistanceUnit.INCH);
+                setAllDrivePower(0.5,0.5,-0.5,-0.5);
+            }
 
         }
 
