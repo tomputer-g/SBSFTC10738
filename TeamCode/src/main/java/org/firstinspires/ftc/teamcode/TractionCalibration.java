93139d54c6d5ea3e62bbecdb114866fc9ccb83a4
soxehli <fangzhong.lyu@sbs.org>
2019-10-24 09:28:46 -0400

    Update TractionCalibration.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TractionCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TractionCalibration.java
@@ -9,7 +9,7 @@ public class TractionCalibration extends BaseAuto{
         cLF = Math.abs(getMC(LF)-pcLF); cLB = Math.abs(getMC(LB)-pcLB); cRF = Math.abs(getMC(RF)-pcRF); cRB = Math.abs(getMC(RB)-pcRB);
         min=Math.min(cLF,Math.min(cLB,Math.min(cRF,cRB)));
         minp=Math.min(pLF,Math.min(pLB,Math.min(pRF,pRB)));
-        setAllDrivePower1(cLF/min>pLF/minp?0:pLF, cLB/min>pLB/minp?0:pLB, cRF/min>pRF/minp?0:pRF, cRB/min>pRB/minp?0:pRB);
+        setAllPDrivePower1(cLF/min>pLF/minp?0:pLF, cLB/min>pLB/minp?0:pLB, cRF/min>pRF/minp?0:pRF, cRB/min>pRB/minp?0:pRB);
         pcLF=cLF; pcLB=cLB; pcRF=cRF; pcRB=cRB;
     }
     protected double getMC(DcMotor i){
