d7bf33c4e0b72ca0fcc25ddac07bf51e2ff9b4b8
soxehli <fangzhong.lyu@sbs.org>
2019-11-14 17:14:52 -0500

    as

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/BrakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/BrakeTest.java
@@ -9,13 +9,13 @@ import org.firstinspires.ftc.teamcode20.TractionControl;
 @TeleOp
 public class BrakeTest extends TractionControl {
     private boolean[] a={true},b={true},c={true},d={true};
-    private double runSpeed =0.5,brakeSpeed=0.1,start,end;
+    private double runSpeed =0.5,brakeSpeed=0.2,start,end;
     int phase=0;
     ElapsedTime t;
     @Override
     public void init(){
         initDrivetrain();
-        setMode_RUN_WITH_ENCODER();
+        setMode_RUN_WITHOUT_ENCODER();
         t= new ElapsedTime();
     }
     @Override
@@ -35,31 +35,26 @@ public class BrakeTest extends TractionControl {
 
     @Override
     public void loop(){
-        if(整(this.gamepad1.right_bumper,c)){
-            phase++;
-            //end=0;start=0;
-        }
-        if(整(this.gamepad1.left_bumper,d)){
-            phase--;
-            //end=0;start=0;
-        }
+        if(整(this.gamepad1.right_bumper,c))phase++;
+        if(整(this.gamepad1.left_bumper,d)) phase--;
         if(phase==0) {
             telemetry.addLine("IDLE");
             setAllDrivePower(0);
-        }else if(phase==1) {
+        }
+        else if(phase==1) {
             setAllDrivePower(-runSpeed, -runSpeed, runSpeed, runSpeed);
             telemetry.addData("MOVING AT ",runSpeed);
             telemetry.addData("actual", LF.getPower());
             telemetry.addData("encoder", LF.getCurrentPosition());
-        }else if(phase>1){
+        }
+        else if(phase>1){
             telemetry.addData("BRAKING AT ",brakeSpeed);
-            //start = t.milliseconds();
-            brakeTD(brakeSpeed,0);
-            //end=t.milliseconds();
+            start = t.milliseconds();
+            brakeTD(brakeSpeed,-1);
+            end=t.milliseconds();
             phase = 0;
         }
-        telemetry.addData("phase",phase);
-        telemetry.update();
-        //if(end-start!=0)telemetry.addData("Brake Time:",end-start);
+        telemetry.addData("PHASE ",phase);
+        telemetry.addData("Brake Time:",end-start);
     }
 }
