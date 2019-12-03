8203f9c8de0b74fe5fad09888cea3f1e6454ebca
TomGao2001 <gzm2001@outlook.com>
2019-12-03 12:32:06 -0500

    stuf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/BlueAuto.java
@@ -3,13 +3,24 @@ package org.firstinspires.ftc.teamcode20;
 import org.firstinspires.ftc.teamcode20.BaseAuto;
 
 public class BlueAuto extends BaseAuto {
+
+
     @Override
     public void init() {
-        super.init();
+        initDrivetrain();
+        initIMU();
+        initGrabber();
+        initLinSlide();
+        initVuforiaWebcam();
     }
 
     @Override
     public void loop() {
-        super.loop();
+        //go forward 1 floor mat (24")
+        //vuforia - recognize block & move to pick up
+        //after pickup: turn 90 deg. move to platform, drop off
+        //move to platform, drag into position, release
+        //repeat until run out of time; first on other skystones
+
     }
 }
