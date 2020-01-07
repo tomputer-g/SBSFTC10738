ce89901886d1d33a9df48a109b85251ce3ba5588
LuciEN <lucien.liu@users.noreply.github.com>
2020-01-07 16:29:05 -0500

    Update BlueAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/BlueAuto.java
@@ -11,7 +11,6 @@ import org.firstinspires.ftc.teamcode20.BaseAuto;
 @Autonomous
 public class BlueAuto extends TractionControl {
     private double speed = 0.4;
-    private boolean[] ca={true},cb={true};
     @Override
     public void init() {
         telemetryOn = false;
@@ -24,7 +23,6 @@ public class BlueAuto extends TractionControl {
         initSensors();
         setNewGyro0();
         speed=0.3;
-        //if(telemetryOn)telemetry.setAutoClear(false);
     }
     @Override
     public void loop() {
@@ -33,21 +31,20 @@ public class BlueAuto extends TractionControl {
         //after pickup: turn 90 deg. move to platform, drop off
         //move to platform, drag into position, release
         //repeat until run out of time; first on other skystones
+
+        //initialization
         grabber_extender.setPower(1);
         wait(400);
-        grabber_extender.setPower(0);
+        grabber_extender.setPower(0.0);
         grabber.setPosition(grabber_open);
         platform_grabber.setPower(1);
         wait(300);
-        platform_grabber.setPower(0);
+        platform_grabber.setPower(0.0);
         moveInchesG(0,15,0.3);
         if(telemetryOn)telemetry.clear();
         int pos = skystonePosition();
         shutdownVuforia();
-        telemetry.addData("pos: ",pos);
-        telemetry.update();
 
-        //pos = 1;
         int shift;
         if(pos == 1){shift = 0;}
         else if (pos == 0){
@@ -58,7 +55,8 @@ public class BlueAuto extends TractionControl {
             moveInchesG(8, 0, 0.4);
             shift=-8;
         }
-        //move to blocc
+
+        //move to the first blocc
         ElapsedTime p = new ElapsedTime();
         reset_ENCODER();
         setMode_RUN_WITHOUT_ENCODER();
@@ -66,17 +64,22 @@ public class BlueAuto extends TractionControl {
             setAllDrivePowerG(-0.25, -0.25, 0.25, 0.25);
         }
         wait(600);
+
+        //grab 1st block
         grabber.setPosition(.55);
-        setAllDrivePower(0);
+        setAllDrivePower(0.0);
+
+        //move back
         moveInchesG(0,-8,0.3);
+
+        //move forward & approach foundation
         turn(90, 0.4, 4);
         setNewGyro(90);
         p.reset();
         while(21.4<rangeSensorFront.getDistance(DistanceUnit.INCH)||p.milliseconds()<3000){
             setAllDrivePowerG(-speed,-speed,speed,speed);
         }
-        setAllDrivePower(0);
-        //moveInchesG(18,0,.35)
+        setAllDrivePower(0.0);
         setAllDrivePowerG(-.35,.35,-.35,.35);
         wait(1500);
 
@@ -84,29 +87,32 @@ public class BlueAuto extends TractionControl {
         //move foundation
         platform_grabber.setPower(-.8);
         wait(200);
-
         turn(90, 0.67, 5);
-        //while (!near(getHeading(),90,3)) setAllDrivePower(-0.6,0.2,0.8,-0.4);
+        /*
+        //winston attempt
+        while (!near(getHeading(),90,3)) setAllDrivePower(-0.6,0.2,0.8,-0.4);
+        */
+
+        //drag foundation
         setNewGyro(180);
-        //ElapsedTime p = new ElapsedTime();
         double koe=1;
         while(13<rangeSensorFront.getDistance(DistanceUnit.INCH)){
             setAllDrivePowerG(koe*(0.25-0.55+0.37),koe*(0.25-0.55-0.37),koe*(0.25+0.55+0.37),koe*(0.22+0.5-0.37)); //turn+f0rwrd+side
-            //telemetry.addData("Front",rangeSensorFront.getDistance(DistanceUnit.INCH));
-            //telemetry.update();
         }
-        //setAllDrivePowerG(0);
-        //wait(1000);
-        setAllDrivePower(0);
+        setAllDrivePower(0.0);
+
+        //align to the right wall
         while(30>rangeSensorFront.getDistance(DistanceUnit.INCH)){
             //telemetry.addData("Side",rangeSensorSide.getDistance(DistanceUnit.INCH));
             //telemetry.update();
             setAllDrivePowerG(0.5,-0.5,0.5,-0.5);
         }
-        setAllDrivePower(0);
+        setAllDrivePower(0.0);
+
+        //turn and drop the block
         platform_grabber.setPower(1);
         wait(300);
-        platform_grabber.setPower(0);
+        platform_grabber.setPower(0.0);
         moveInchesG(-2.3,0,0.4);
         turn(-90,0.5,8);
         L1.setPower(-0.35);
@@ -114,16 +120,17 @@ public class BlueAuto extends TractionControl {
         wait(500);
         grabber_extender.setPower(-1);
         wait(800);
-        setAllDrivePower(0);
-        grabber_extender.setPower(0);
-        L1.setPower(0);
-        L2.setPower(0);
+        setAllDrivePower(0.0);
+        grabber_extender.setPower(0.0);
+        L1.setPower(0.0);
+        L2.setPower(0.0);
         grabber.setPosition(grabber_open);
         L1.setPower(0.35);
         L2.setPower(-0.35);
-        wait(1000);
-        L1.setPower(0);
-        L2.setPower(0);
+        wait(1200);
+        L1.setPower(0.0);
+        L2.setPower(0.0);
+
         //park
         setNewGyro(90);
         moveInchesG(0, -19, 0.5);
