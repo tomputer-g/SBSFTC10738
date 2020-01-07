4101cec68738d6c1b03ea2c48ad4e518b55030f7
TomGao2001 <gzm2001@outlook.com>
2020-01-07 13:01:15 -0500

    comments & logic & removed 'A'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/TeleOp_MultiThreadDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/TeleOp_MultiThreadDrive.java
@@ -61,11 +61,10 @@ public class TeleOp_MultiThreadDrive extends BaseAuto {
             slow = !slow;
         }
 
-
+        //platform grabber toggle
         if(this.gamepad1.dpad_right) {
             DPRPrimed = true;
-        }
-        if(!this.gamepad1.dpad_right && DPRPrimed){
+        }if(!this.gamepad1.dpad_right && DPRPrimed){
             DPRPrimed = false;
             if(platformGrabbed){//already held
                 platformGrabbed = false;
@@ -78,7 +77,7 @@ public class TeleOp_MultiThreadDrive extends BaseAuto {
             }
         }
 
-
+        //servo toggle
         if(this.gamepad1.b){BPrimed = true;}if(!this.gamepad1.b && BPrimed){BPrimed = false;
             if(grabber.getPosition() > (grabber_closed+grabber_open)/2){
                 grabber.setPosition(grabber_open);
@@ -87,27 +86,21 @@ public class TeleOp_MultiThreadDrive extends BaseAuto {
             }
         }
 
-        if(this.gamepad1.dpad_up
-                ||this.gamepad1.dpad_down
-                ||this.gamepad1.right_bumper
-                ||(this.gamepad1.left_bumper && !near(this.gamepad1.right_stick_y, 0, 0.05))){
-
+        //driver cancel LT&RT (by dpad up/dpad down/ RB/ LB+Left stick
+        if(this.gamepad1.dpad_up ||this.gamepad1.dpad_down ||this.gamepad1.right_bumper ||(this.gamepad1.left_bumper && !near(this.gamepad1.right_stick_y, 0, 0.05))){
             RTState = -1; //driver interrupt auto movement
             autoPlaceState = -1;
         }
 
-        if(this.gamepad1.right_trigger > 0.3
-                && (slideEncoderTravel > 0? L1.getCurrentPosition() < (slideEncoderTravel - 12 * slideEncoderPerInch) : L1.getCurrentPosition() > (slideEncoderTravel - 12 * slideEncoderPerInch))
-                && grabber_extender.getCurrentPosition() < extenderTravel/2
-                && RTState == -1){
+        //RT if RT not started - cancels LT
+        if(this.gamepad1.right_trigger > 0.3 && (slideEncoderTravel > 0? L1.getCurrentPosition() < (slideEncoderTravel - 12 * slideEncoderPerInch) : L1.getCurrentPosition() > (slideEncoderTravel - 12 * slideEncoderPerInch)) && grabber_extender.getCurrentPosition() < extenderTravel/2 && RTState == -1){
             //when can go 12in above & extender is extended & not started
             holdSet = false;
+            autoPlaceState = -1;
             RTState = 0;
         }
-        runSlide();
-        autoPlace();
-        handleRTState();
 
+        //RB toggle extender positions
         if(this.gamepad1.right_bumper){RBPrimed = true;}if(!this.gamepad1.right_bumper && RBPrimed){RBPrimed = false;
             movingExtender = true;
             grabber_extender.setPower(1);
@@ -117,9 +110,9 @@ public class TeleOp_MultiThreadDrive extends BaseAuto {
                 grabber_extender.setTargetPosition(0);
             }
             grabber_extender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }//if RB toggle positions
+        }
 
-
+        //DUP/DDOWN manual extender movement
         if(this.gamepad1.dpad_up){
             movingExtender = false;
             grabber_extender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -142,22 +135,26 @@ public class TeleOp_MultiThreadDrive extends BaseAuto {
             }
         }
 
-        if(this.gamepad1.a){
-            grabber_extender.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            grabber_extender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-
+        //X brake (nobody cares)
         if(zheng(this.gamepad1.x,xprime)) {
             brake();//TODO: blocks thread up to 440 ms
         }
 
+        //If not PWM: run full speed
         if(!slow){
             scaledMove(-this.gamepad1.left_stick_x,-this.gamepad1.left_stick_y, (this.gamepad1.left_bumper?0:-this.gamepad1.right_stick_x));
         }
 
+        //LT
         if(this.gamepad1.left_trigger  > .5 && autoPlaceState == -1){//dependent on other things?
             autoPlaceState = 0;
+            RTState = -1;
         }
+
+        //run LT, RT, normal control
+        runSlide();
+        autoPlace();
+        handleRTState();
 
         if(telemetryOn) {
             telemetry.addData("RT state", RTState);
@@ -178,7 +175,7 @@ public class TeleOp_MultiThreadDrive extends BaseAuto {
         @Override
         public void run() {
             while(!isInterrupted()&&!stop){
-                if(slow){//what about slide?
+                if(slow){
                     setAllDrivePowerSlow(-1*(int)gamepad1.left_stick_y,(int)(gamepad1.left_stick_x),-1*(int)(gamepad1.right_stick_x));
                 }
             }
