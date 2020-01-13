4375271152d0ac1c114af4f3eb3243b9b7160525
LuciEN <lucien.liu@users.noreply.github.com>
2020-01-13 15:00:39 -0500

    Update SpinTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/SpinTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/SpinTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode20.Tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -8,7 +9,7 @@ import org.firstinspires.ftc.teamcode20.BaseAuto;
 
 @TeleOp
 public class SpinTest extends BaseAuto {
-    private boolean a[]={true}, b[]={true};
+    private boolean a[]={true}, b[]={true}, aa[]={true};
     private double last_imu, d, speed;
     ElapsedTime p  =new ElapsedTime();
 
@@ -22,6 +23,7 @@ public class SpinTest extends BaseAuto {
 
     @Override
     public void loop() {
+        /*
         setAllDrivePower(speed);
 
         if(p.milliseconds() >1000){
@@ -31,11 +33,28 @@ public class SpinTest extends BaseAuto {
             }
             last_imu = getHeading();
         }
+         */
+        telemetry.addData("speed", "%.3f", speed);
         if(zheng(this.gamepad1.x, a)){
             speed +=0.05;
         }
         if (zheng(this.gamepad1.y,b)){
             speed-=0.05;
         }
+        if(zheng(this.gamepad1.a,aa)){
+            setAllDrivePower(-speed,-speed,speed,speed);
+            LF.setTargetPosition((int) xmult*48);
+            LB.setTargetPosition((int) xmult*48);
+            RF.setTargetPosition((int) xmult*48);
+            RB.setTargetPosition((int) xmult*48);
+            LF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            LB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            while (LF.isBusy() || LB.isBusy() || RF.isBusy() || RB.isBusy());
+            setAllDrivePower(0);
+        }
+        telemetry.update();
+
     }
 }
