5944d626a32f0124150595989c75c0ba5893b9c8
ImagTachyon <44809267+JerryWan83274@users.noreply.github.com>
2020-01-13 19:23:09 -0500

    xdf

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/SoundTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode20.Tests;
+
+import android.media.MediaPlayer;
+import com.qualcomm.ftccommon.SoundPlayer;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.R;
+import org.firstinspires.ftc.teamcode20.BaseAuto;
+
+@TeleOp
+public class SoundTest extends BaseAuto {
+    private int soundID = hardwareMap.appContext.getResources().getIdentifier("sound1", "raw", hardwareMap.appContext.getPackageName());
+    private boolean bf[]={true};
+    @Override
+    public void init(){
+    }
+    @Override
+    public void loop(){
+        if(zheng(this.gamepad1.right_bumper,bf))
+            SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, soundID);
+    }
+}
