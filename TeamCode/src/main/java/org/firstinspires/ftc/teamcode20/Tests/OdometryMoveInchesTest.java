db454469fb44d865c17abc2161ff6fe0155dbd12
soxehli <fangzhong.lyu@sbs.org>
2020-01-28 12:31:10 -0500

    Revert "Merge branch 'master' into Barry"
    This reverts commit 8962d7b53b32a8a43a222958321d0d6950f4f97e, reversing
    changes made to 713bb026ce5448414f64a5e700facc915679f57d.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/OdometryMoveInchesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/OdometryMoveInchesTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode20.Tests;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -34,7 +36,7 @@ public class OdometryMoveInchesTest extends BaseAuto {
     @Override
     public void loop() {
         if(this.gamepad1.a){APrimed = true;}if(APrimed && !this.gamepad1.a){ APrimed = false;
-            resetY1Odometry();
+            resetYOdometry();
             resetXOdometry();
             LF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             LF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -121,7 +123,7 @@ public class OdometryMoveInchesTest extends BaseAuto {
             writeLog(t.milliseconds()+", "+currentOdometry+", "+((currentOdometry - odometryXGoal)/odometryEncXPerInch)+", "+((currentOdometry - odometryXGoal)/odometryEncXPerInch)*params[0]+", "+Dterm+", "+(near(Dterm,0,speed * 5000 / 0.3)?(params[2] * Dterm):"CLIPPED")+", "+LF.getPower()+", "+LF.getCurrentPosition()+", "+getHeading());
         }
         setAllDrivePower(0);
-        writeLogHeader("Gyro drift="+getHeading()+", Ydrift="+ getY1Odometry());
+        writeLogHeader("Gyro drift="+getHeading()+", Ydrift="+getYOdometry());
         writeLogHeader("----End of GOX----");
     }
     private void antiSkidAccelerationX(double start, double goal, double accTime){
