f5cde3920b136a82779d3f230e9738cfb0006eb1
soxehli <fangzhong.lyu@sbs.org>
2020-02-04 13:27:07 -0500

    asd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/Straighttest.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode20.Tests;
+
+import com.acmerobotics.dashboard.config.Config;
+
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+
+import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveBase;
+
+import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveREV;
+
+
+@Config
+
+@Autonomous(group = "drive")
+
+public class StraightTest extends LinearOpMode {
+
+    public static double DISTANCE = 60;
+
+
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+
+        SampleMecanumDriveBase drive = new SampleMecanumDriveREV(hardwareMap);
+
+
+
+        Trajectory trajectory = drive.trajectoryBuilder()
+
+                .forward(DISTANCE)
+
+                .build();
+
+
+
+        waitForStart();
+
+
+
+        if (isStopRequested()) return;
+
+
+
+        drive.followTrajectorySync(trajectory);
+
+    }
+
+}
