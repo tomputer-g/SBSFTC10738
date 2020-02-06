8b4f734df0e59d882df7dcc76753da1a50129cb6
soxehli <fangzhong.lyu@sbs.org>
2020-02-06 09:36:49 -0500

    asd

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaSkyStoneNavigation.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaSkyStoneNavigation.java
@@ -341,9 +341,11 @@ public class ConceptVuforiaSkyStoneNavigation extends LinearOpMode {
             if (targetVisible) {
                 // express position (translation) of robot in inches.
                 VectorF translation = lastLocation.getTranslation();
+                double x = translation.get(0) / mmPerInch-;
+                double y = translation.get(1) / mmPerInch+9;
                 telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                        translation.get(0) / mmPerInch-6.5, translation.get(1) / mmPerInch+9, translation.get(2) / mmPerInch);
-                telemetry.addLine("Distance to back up");
+                        x,y, translation.get(2) / mmPerInch);
+                telemetry.addLine("Distance to back up: ");
 
                 // express the rotation of the robot in degrees.
                 Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
