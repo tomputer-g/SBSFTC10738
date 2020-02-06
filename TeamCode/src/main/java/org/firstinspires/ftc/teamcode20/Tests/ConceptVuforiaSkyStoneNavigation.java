9b55af84acbd16f58f13a7df2d9ce4f1723faa78
TomGao2001 <gzm2001@outlook.com>
2020-02-06 09:36:01 -0500

    ASEtr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/ConceptVuforiaSkyStoneNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/ConceptVuforiaSkyStoneNavigation.java
@@ -27,14 +27,12 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode20.Tests;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
