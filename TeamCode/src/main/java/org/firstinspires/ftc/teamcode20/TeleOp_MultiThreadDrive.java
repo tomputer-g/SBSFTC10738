c967bc018261265dd69a70195b30f6a9e1beff84
ImagTachyon <44809267+JerryWan83274@users.noreply.github.com>
2020-02-13 10:02:34 -0500

    asd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/TeleOp_MultiThreadDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/TeleOp_MultiThreadDrive.java
@@ -115,7 +115,7 @@ public class TeleOp_MultiThreadDrive extends BaseAuto {
 
         if(this.gamepad1.start){start = true;}if(start && !this.gamepad1.start){
             start = false;
-            if(france.getPosition() >.5){
+            if(france.getPosition() >.25){
                 france.setPosition(0);
             }else{
                 france.setPosition(.5);
