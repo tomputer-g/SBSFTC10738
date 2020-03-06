283e56ca1cea9ebe1caed239a04abe541a6e5878
soxehli <fangzhong.lyu@sbs.org>
2020-03-06 16:32:08 -0500

    asd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/RedAutoPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/RedAutoPark.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode20;
 
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -11,12 +13,24 @@ import org.openftc.revextensions2.ExpansionHubEx;
 @Autonomous
 public class RedAutoPark extends BaseAuto{
     @Override public void runOpMode() throws InterruptedException {
-        initAutonomous();
+        showTelemetry = false;
+        initDrivetrain();//181.64ms
+        initIMU();//!!!!!1.259s : stops if thread has Interrupt flag
+        initGrabber();//1.14ms
+        initPlatformGrabber();//34.20ms
+        initOdometry();//100.89ms
+        xOdometryEnableServo.setPosition(xOdoEnable);
+        setNewGyro0();
+        initHubs();
+        //initVuforia();
+        //initViewMarks();
+        Log.i("Auto init", "done");
         while (!isStarted()) {
             Thread.sleep(200);
         }
-        before_start();
-        moveInchesGOXT(8,0.8,1,1200);
+        //before_start();
+        moveInchesGOXT(10,0.8,1,1500);
         requestOpModeStop();
+
     }
 }
