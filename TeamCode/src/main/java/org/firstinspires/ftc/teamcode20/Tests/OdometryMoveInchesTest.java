577116360b719a242911349e6fcee98154e0fef7
TomGao2001 <gzm2001@outlook.com>
2020-03-10 13:41:28 -0400

    Update OdometryMoveInchesTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/OdometryMoveInchesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/OdometryMoveInchesTest.java
@@ -17,14 +17,15 @@ public class OdometryMoveInchesTest extends BaseAuto {
     0.6 speed: P = 0.075,   D = 1.4E-2, result: 0 in.
     0.9 speed: P = 0.0325,  D = 7.3E-3, result: spin +- 1/4 in
      */
-    private double[] params =       {0,  0,     0,       0.9,        90,           0.915};
-    private String[] paramNames =   {"P",   "I",    "D",    "speed",    "targetInches","k"};
-    private int currentSelectParamIndex = 3;
+    private double[] params =       {0.0325,  0,     7.3E-3};//,  0.9,        90,           0.915};
+    private String[] paramNames =   {"P",   "I",    "D"};//,    "speed",    "targetInches","k"};
+    private int currentSelectParamIndex = 0;
     private boolean l, r, u, d, lb, rb, y, APrimed = false, x = false, platformGrabbed = false;
 
+    private double displayMS = 0;
     protected final double odometryEncYPerInch = 1324.28, odometryEncXPerInch = 1316.38;
 
-    @Override //chiggas
+    @Override
     public void runOpMode() throws InterruptedException {
         initHubs();
         initDrivetrain();
@@ -40,7 +41,10 @@ public class OdometryMoveInchesTest extends BaseAuto {
                 setNewGyro0();
                 LF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 LF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                moveInchesGOY_XF(params[4], params[3],params[5]);
+                ElapsedTime t = new ElapsedTime();
+                //moveInchesYSpin(25,90,0.5);
+                //moveInchesGOY_XFixed(90,0.9,1,getXOdometry()); 0.96 deg in 2.35s
+                displayMS = t.milliseconds();
             }
 
             if(this.gamepad1.left_bumper){lb = true;}if(!this.gamepad1.left_bumper && lb){
@@ -96,13 +100,124 @@ public class OdometryMoveInchesTest extends BaseAuto {
                     platform_grabber.setPower(-0.4);
                 }
             }
-            telemetry.addData("parameters",params[3]+", "+params[4]+","+params[5]);
+            telemetry.addData("parameters",params[0]+", "+params[1]+","+params[2]);
             telemetry.addData("now changing", paramNames[currentSelectParamIndex]);
             telemetry.addData("enc X", getXOdometry());
             telemetry.addData("enc Y1",getY1Odometry());
+            telemetry.addData("imu",getHeading());
+            telemetry.addData("last run time",displayMS);
             telemetry.update();
         }
         stopLog();
+    }
+
+    protected void setAllDrivePowerPolar(double r, double theta){
+        LF.setPower(r* (-Math.sin(theta)-Math.cos(theta)));
+        LB.setPower(r* (Math.sin(theta)-Math.cos(theta)));
+        RF.setPower(r* (-Math.sin(theta)+Math.cos(theta)));
+        RB.setPower(r* (Math.sin(theta)+Math.cos(theta)));
+    }
+
+    /*protected void moveInchesYSpin(double yInch, double angle, double speed,double a,double b,double c,double d,boolean resetOffset) throws InterruptedException {
+        double turnkP = 0.068, turnkD = 0.9;
+        double movekP = 0.1, movekD = 0.02;
+        double turnSpd = 0.5, moveSpd = 0.5;
+
+        //input processing
+        yInch = -yInch;
+        speed = Math.abs(speed);
+        int steadyCounter = 0;if(resetOffset){
+            acctarget=0;
+            setNewGyro0();
+        }
+        double e = target;
+        ElapsedTime t = new ElapsedTime();
+        ElapsedTime n = new ElapsedTime();
+        int i=0;
+        while(i<5&&n.milliseconds()<((speed>0.5)?800:2000)){
+            Thread.sleep(0);
+            double e2 = target-(getAdjustedHeading(target));
+            double D = kd*(e2-e)/t.milliseconds();
+            t.reset();
+            double P = e2*kp;
+            if(Math.abs(P)>Math.abs(speed))P=P>0?speed:-speed;
+            setAllDrivePower(P+D);
+            e=e2;
+            if(near(e2-e,0,0.3)&&near(e2,0,3))
+                i++;
+        }
+        setAllDrivePower(0);
+        acctarget+=target;
+        if(resetOffset) {
+            acctarget = 0;
+            setNewGyro0();
+        }
+        else
+            setNewGyro(acctarget);
+
+        //shortcut because dumbass
+    }
+     */
+
+
+    protected void moveInchesGOY_XFixed(double yInch, double speed,double kV, int FixXOffset) throws InterruptedException {//use 0.4 for short-dist
+        yInch = -yInch;
+        //setNewGyro0();
+        double kP = params[0], kD = params[2];
+        if(yInch == 0)return;
+        //0.25 0.005
+        double kPx = 0.2, kDx = 0.005;
+        ElapsedTime t = new ElapsedTime();
+        int offsetY = getY1Odometry();
+        int offsetX = FixXOffset;
+        double diff = 0;
+        speed=Math.abs(speed);
+        double multiply_factor, prev_speed = 0;
+        int odometryYGoal = offsetY + (int)(yInch * odometryEncYPerInch);
+        double vy = speed;
+        int previousPos = offsetY, previousPosX = offsetX, currentOdometry = offsetY, currentOdometryX, Dterm, DtermX;
+        double tpre = 0, tcur;
+        int steadyCounter = 0;
+
+        double rampupCurrentSpeed = 0.2;
+        while(rampupCurrentSpeed < 0.9){
+            tcur = t.milliseconds();
+            rampupCurrentSpeed += vy * 0.1;
+            telemetry.addData("Rampup",rampupCurrentSpeed);
+            telemetry.update();
+            currentOdometryX = getXOdometry();
+
+            setAllDrivePowerG(-rampupCurrentSpeed,-rampupCurrentSpeed,rampupCurrentSpeed,rampupCurrentSpeed,1.4);
+            previousPosX = currentOdometryX;
+            tpre = tcur;
+        }
+        while(steadyCounter < 3){
+            //telemetry.addData("x",getXOdometry());
+            //telemetry.addData("yL",getY1Odometry());
+            //telemetry.addData("yR",getY2Odometry());
+            //telemetry.update();
+            Thread.sleep(0);
+
+            currentOdometry = getY1Odometry();
+            currentOdometryX = getXOdometry();
+            tcur=t.milliseconds();
+            Dterm = (int)((currentOdometry - previousPos)/(tcur-tpre));
+            DtermX = (int)((currentOdometryX - previousPosX)/(tcur-tpre));
+            diff = (currentOdometryX - offsetX)/odometryEncXPerInch*kPx + (near(DtermX,0,speed * 5000 / 0.3)?(kDx *DtermX):0);
+            multiply_factor = -Math.min(1, Math.max(-1, kV*((kP * (currentOdometry - odometryYGoal)/ odometryEncYPerInch) +  (near(Dterm,0,speed * 5000 / 0.3)?(kD * Dterm):0))));
+            if(near(prev_speed, multiply_factor*vy,0.001) && near(prev_speed, 0, 0.1)){
+                steadyCounter++;
+            }else{
+                steadyCounter = 0;
+            }
+            Log.d("GOY "+yInch,"steady"+steadyCounter+", position"+currentOdometry+", LF speed"+prev_speed+", OC speed="+Dterm+"bulkSpd="+hub4.getBulkInputData().getMotorVelocity(platform_grabber));
+            previousPos = currentOdometry;
+            previousPosX = currentOdometryX;
+            tpre=tcur;
+            setAllDrivePowerG(multiply_factor*vy + diff,multiply_factor*vy - diff,multiply_factor*-vy + diff,multiply_factor*-vy - diff,1.4);
+            prev_speed = multiply_factor * vy;
+        }
+        setAllDrivePower(0);
     }
 
 }
