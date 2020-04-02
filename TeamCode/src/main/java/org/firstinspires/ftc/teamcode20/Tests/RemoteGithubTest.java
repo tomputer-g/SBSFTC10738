243401c9e5e0629d93e4fc7d71846ec91b22f79d
TomGao2001 <gzm2001@outlook.com>
2020-04-02 10:38:08 -0400

    Create RemoteGithubTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode20/Tests/RemoteGithubTest.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode20.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode20.BaseOpMode;
+@TeleOp
+public class RemoteGithubTest extends BaseOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
